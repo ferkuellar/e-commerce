84805a6b5acc48b57b62d8cc171f0ecdb9c6924c
fercuellar <fercuellar@gmail.com>
2023-07-16 12:00:58 -0600

    listo

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
+
 const PrivateRoute = ({ children }) => {
   const { user } = useAuth0();
   if (!user) {
